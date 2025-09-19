f507caad2c6315b8fd318456f70b519dd3fd3763
Gustavo Arruda <garrudas@gmail.com>
2025-09-19 13:23:58 -0300

    Interface - Componente de Dica

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from "./app.module.css"
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
 
 export function App() {
   function handleRestartGame() {
@@ -10,6 +11,7 @@ export function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Uma das linguagens de programação mais utilizadas" />
       </main>
     </div>
   )
