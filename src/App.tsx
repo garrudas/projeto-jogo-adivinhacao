4a71dd33733b98f8c08b007bcc70a4a0a5f76b04
Gustavo Arruda <garrudas@gmail.com>
2025-09-20 11:43:31 -0300

    Interface - Criando Componente de Input

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./app.module.css"
 import { Header } from "./components/Header"
 import { Letter } from "./components/Letter"
 import { Tip } from "./components/Tip"
+import { Input } from "./components/Input"
 
 export function App() {
   function handleRestartGame() {
@@ -19,6 +20,12 @@ export function App() {
           <Letter value="A" />
           <Letter value="C" />
           <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
         </div>
       </main>
     </div>
