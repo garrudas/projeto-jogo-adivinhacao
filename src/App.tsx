3cf51be38dc4a75a8f365d7bb328415a8d79aa77
Gustavo Arruda <garrudas@gmail.com>
2025-09-21 12:20:17 -0300

    Funcionalidades - Criando a handleConfirm

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,27 @@ export function App() {
     setLetter("")
   }
 
+  function handleConfirm() {
+    if (!challenge) {
+      return
+    }
+
+    if (!letter.trim()) {
+      return alert("Digite uma letra!")
+    }
+
+    const value = letter.toUpperCase()
+    const exists = lettersUsed.find((used) => used.value.toUpperCase() === value)
+
+    if (exists) {
+      return alert("Você já utilizou a letra " + value)
+    }
+
+    setLettersUsed((prevState) => [...prevState, { value, correct: false }])
+
+    setLetter("")
+  }
+
   useEffect(() => {
     startGame()
   }, [])
@@ -52,8 +73,14 @@ export function App() {
         <h4>Palpite</h4>
 
         <div className={styles.guess}>
-          <Input autoFocus maxLength={1} placeholder="?" />
-          <Button title="Confirmar" />
+          <Input
+            autoFocus
+            maxLength={1}
+            placeholder="?"
+            value={letter}
+            onChange={(e) => setLetter(e.target.value)}
+          />
+          <Button title="Confirmar" onClick={handleConfirm} />
         </div>
 
         <LettersUsed data={lettersUsed} />
