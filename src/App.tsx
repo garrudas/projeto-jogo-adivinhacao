0aa9d8f570e4e598402cbeefad1166a983af14e4
Gustavo Arruda <garrudas@gmail.com>
2025-10-25 12:54:15 -0300

    Deploy Front-end - Build de preview

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ export function App() {
   const [letter, setLetter] = useState("")
   const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
   const [challenge, setChallenge] = useState<Challenge | null>(null)
+  const [shake, setShake] = useState(false)
 
   function handleRestartGame() {
     const isConfirmed = window.confirm("Você tem certeza que deseja reiniciar?")
@@ -63,6 +64,11 @@ export function App() {
     setLettersUsed((prevState) => [...prevState, { value, correct }])
     setScore(currentScore)
     setLetter("")
+
+    if (!correct) {
+      setShake(true)
+      setTimeout(() => setShake(false), 300)
+    }
   }
 
   function endGame(message: string) {
@@ -109,7 +115,7 @@ export function App() {
 
         <Tip tip={challenge.tip} />
 
-        <div className={styles.word}>
+        <div className={`${styles.word} ${shake && styles.shake}`}>
           {challenge.word.split("").map((letter, index) => {
             const letterUsed = lettersUsed.find(
               (used) => used.value.toUpperCase() === letter.toUpperCase()
